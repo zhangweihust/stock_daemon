88313be18542492fdd91e287db32b5c4fb5b102a
zhangwei <zhangweihust@gmail.com>
2013-06-15 17:15:50 +0800

    println

--- stock_daemon/src/com/zhangwei/stock/androidconvert/Log.java
+++ stock_daemon/src/com/zhangwei/stock/androidconvert/Log.java
@@ -3,23 +3,23 @@ package com.zhangwei.stock.androidconvert;
 public class Log {
 
 	public static void  e(String tag, String exp){
-		System.err.print("[" + tag + "]:" + exp);
+		System.err.println("[" + tag + "]:" + exp);
 	}
 	
 	public static void  w(String tag, String exp){
-		System.err.print("[" + tag + "]:" + exp);
+		System.err.println("[" + tag + "]:" + exp);
 	}
 	
 	public static void  i(String tag, String exp){
-		System.err.print("[" + tag + "]:" + exp);
+		System.err.println("[" + tag + "]:" + exp);
 	}
 	
 	public static void  d(String tag, String exp){
-		System.err.print("[" + tag + "]:" + exp);
+		System.err.println("[" + tag + "]:" + exp);
 	}
 	
 	public static void  v(String tag, String exp){
-		System.err.print("[" + tag + "]:" + exp);
+		System.err.println("[" + tag + "]:" + exp);
 	}
 
 }
