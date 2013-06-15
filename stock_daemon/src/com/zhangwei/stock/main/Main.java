0e34388703c8d28e0e3a2e610adc7e3fabe75553
zhangwei <zhangweihust@gmail.com>
2013-06-15 17:19:58 +0800

    thread alive check

--- stock_daemon/src/com/zhangwei/stock/main/Main.java
+++ stock_daemon/src/com/zhangwei/stock/main/Main.java
@@ -3,6 +3,7 @@ package com.zhangwei.stock.main;
 import com.zhangwei.stock.service.DailyTask;
 
 public class Main {
+	static DailyTask dt;
 
 	/**
 	 * @param args
@@ -10,8 +11,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		DailyTask dt = new DailyTask();
-		dt.start();
+		if(!dt.isAlive()){
+			dt = new DailyTask();
+			dt.start();
+		}
+
 	}
 
 }
